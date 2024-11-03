d15d6ac3519d3b2e675faf929d79d1cfb0800463
Swapnil Patle <swapnilpatle01@gmail.com>
2024-11-03 10:28:15 +0530

    Added a description in about us page

--- Frontend/src/Components/AboutUs/AboutUs.jsx
+++ Frontend/src/Components/AboutUs/AboutUs.jsx
@@ -1,6 +1,5 @@
 import { Typography, Button, Modal, Row, Col, Card } from "antd";
-import { PlayCircleOutlined } from "@ant-design/icons";
-import { LinkedinOutlined } from "@ant-design/icons";
+import { PlayCircleOutlined, LinkedinOutlined } from "@ant-design/icons";
 import logo from "../../assets/images/aboutus.png";
 import "./AboutUs.css";
 import { Link } from "react-router-dom";
@@ -17,7 +16,9 @@ function AboutUs() {
   const teamMembers = [
     {
       name: "Swapnil Patle",
-      title: "Founder and CEO",
+      title: "Founder",
+      description:
+        "As the full stack developer and founder of Engineer StudyHub, Swapnil Patle is committed to providing future engineers with accessible, high-quality educational resources. With a robust engineering background, he drives the platform's mission to integrate academic knowledge with practical industry skills, ensuring students are well-prepared for their careers.",
       image:
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROrNO-3i-suP7E4U1ujqk67dgi4mkiqiMQBQ&s",
       linkedin: "https://in.linkedin.com/in/swapnil-patle-821534223",
@@ -25,9 +26,29 @@ function AboutUs() {
     {
       name: "Ghamesh Rahangdale",
       title: "Co-Founder",
+      description:
+        "Co-founder and React full stack developer of Engineer StudyHub, Ghamesh Rahangdale plays a pivotal role in the platform's development. His expertise in modern web technologies helps create a seamless learning experience, empowering engineering students with the tools they need for success in their academic and professional journeys.",
       image:
         "https://media.licdn.com/dms/image/v2/D5603AQEBmg-wc9nIAQ/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1679968900193?e=1734566400&v=beta&t=MN5EozGGIpoVFd6oCsK2gFe0DuTtIzBSi6tvUGg-RvA",
       linkedin: "https://in.linkedin.com/in/ghamesh-rahangdale-233753221",
+    },
+    {
+      name: "Prof. Rahul Bhandekar",
+      title: "Guide and Mentor",
+      description:
+        "Prof. Rahul Bhandarakar serves as a mentor for Engineer StudyHub and is the Head of the Computer Science Department at WCEM Nagpur. His guidance ensures that the platform's resources align with academic standards, supporting students in developing the critical skills necessary for their careers.",
+      image:
+        "https://static.wixstatic.com/media/d890ed_8b0ed38afff841dc99be56fb67a7511a~mv2.png/v1/crop/x_0,y_2,w_285,h_251/fill/w_233,h_205,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/image014.png",
+      linkedin: "https://in.linkedin.com",
+    },
+    {
+      name: "Rupendra Girare",
+      title: "Startup Entrepreneur | Mentor",
+      description:
+        "Rupendra Girare is an innovative startup entrepreneur with a passion for cryptocurrency and digital content creation. As a YouTuber, he shares insights and knowledge on emerging technologies and entrepreneurial strategies, inspiring aspiring business leaders. His mentorship plays a vital role in guiding students and entrepreneurs alike, fostering a spirit of creativity and collaboration within the community.",
+      image:
+        "https://media.licdn.com/dms/image/v2/D5603AQE1PRl7imis4w/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1726749964356?e=1736380800&v=beta&t=-k6-y_QQ6fC7eESx_j8EtWH-OZw58ZjYLIIt1qndOww",
+      linkedin: "https://www.linkedin.com/in/rupendragirare/",
     },
   ];
 
@@ -37,19 +58,10 @@ function AboutUs() {
         justify="start"
         align="middle"
         gutter={[16, 16]}
-        style={{
-          margin: "0 auto",
-          padding: "20px 0 20px 40px",
-          // background: "red", add background color
-        }}
+        style={{ margin: "0 auto", padding: "20px 0 20px 40px" }}
       >
         <Col xs={24} md={12}>
-          <Title
-            level={2}
-            style={{
-              color: "#723BDB",
-            }}
-          >
+          <Title level={2} style={{ color: "#723BDB" }}>
             Welcome to Engineer StudyHub: Your Gateway to Engineering Excellence
           </Title>
           <Text
@@ -87,8 +99,6 @@ function AboutUs() {
               marginTop: "20px",
             }}
           />
-
-          {/* Play Button Overlay */}
           <Button
             type="primary"
             shape="circle"
@@ -125,7 +135,6 @@ function AboutUs() {
         ></iframe>
       </Modal>
 
-      {/* Our Mission Section */}
       <div
         style={{
           padding: "40px 20px",
@@ -136,27 +145,12 @@ function AboutUs() {
         <Row
           justify="center"
           align="middle"
-          style={{
-            maxWidth: "1400px",
-            margin: "10px auto",
-          }}
+          style={{ maxWidth: "1400px", margin: "10px auto" }}
           gutter={[16, 16]}
         >
-          {/* Full-width Column for Mission Content */}
-          <Col
-            xs={24}
-            style={{
-              textAlign: "center",
-            }}
-          >
+          <Col xs={24} style={{ textAlign: "center" }}>
             <Title level={1}>Our Mission</Title>
-            <Title
-              level={4}
-              style={{
-                color: "#48485E",
-                fontWeight: 600,
-              }}
-            >
+            <Title level={4} style={{ color: "#48485E", fontWeight: 600 }}>
               Empowering Engineering Excellence
             </Title>
             <Text
@@ -180,7 +174,6 @@ function AboutUs() {
         </Row>
       </div>
 
-      {/* Leadership team section */}
       <div style={{ padding: "40px 20px", backgroundColor: "#f0f2f5" }}>
         <Row
           justify="center"
@@ -191,11 +184,10 @@ function AboutUs() {
           </Col>
         </Row>
 
-        {/* Leader Cards */}
         <Row
           justify="center"
           gutter={[24, 24]}
-          style={{ maxWidth: "1200px", margin: " auto", marginTop: "50px" }}
+          style={{ maxWidth: "1200px", margin: "auto", marginTop: "50px" }}
         >
           {teamMembers.map((member, index) => (
             <Col xs={24} sm={12} md={8} key={index}>
@@ -207,7 +199,7 @@ function AboutUs() {
                   textAlign: "center",
                   overflow: "hidden",
                   boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px",
-                  height: "420px",
+                  // height: "100%",
                   padding: "10px",
                   margin: "12px",
                 }}
@@ -216,147 +208,66 @@ function AboutUs() {
                   style={{
                     display: "flex",
                     justifyContent: "center",
-                    margin: "10px 0",
-                    padding: "10px",
+                    alignItems: "center",
+                    // height: "160px",
+                    overflow: "hidden",
                   }}
                 >
                   <img
-                    alt={member.name}
                     src={member.image}
+                    alt={member.name}
                     style={{
-                      borderRadius: "50%",
-                      width: "110px",
-                      height: "110px",
+                      width: "80px",
+                      height: "80px",
                       objectFit: "cover",
+                      borderRadius: "50%",
+                      border: "2px solid #723BDB",
                     }}
                   />
                 </div>
                 <Title
-                  level={4}
-                  style={{
-                    fontSize: "20px",
-                    marginBottom: "5px",
-                    padding: "5px",
-                    fontWeight: "600",
-                  }}
+                  level={3}
+                  style={{ color: "#723BDB", marginTop: "16px" }}
                 >
                   {member.name}
                 </Title>
                 <Text
-                  type="primary"
                   style={{
-                    marginBottom: "5px",
-                    display: "block",
-                    padding: "4px",
-                    fontWeight: "450",
+                    color: "#737477",
+                    fontSize: "16px",
+                    fontWeight: "500",
                   }}
                 >
                   {member.title}
                 </Text>
-                <div style={{ padding: "30px" }}>
-                  <Link
-                    to={member.linkedin}
-                    style={{
-                      display: "inline-flex",
-                      alignItems: "center",
-                      backgroundColor: "#0077B5",
-                      color: "#fff",
-                      borderRadius: "5px",
-                      padding: "8px 16px",
-                      textDecoration: "none",
-                      fontWeight: "600",
-                      transition: "background-color 0.3s",
-                      boxShadow: "rgba(0, 0, 0, 0.15) 0px 4px 8px",
-                    }}
+                <Text
+                  style={{
+                    marginTop: "6px",
+                    color: "#737477",
+                    fontSize: "13px",
+                    textAlign: "justify",
+                  }}
+                >
+                  {member.description}
+                </Text>
+                <Link
+                  to={member.linkedin}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Button
+                    type="primary"
+                    icon={<LinkedinOutlined />}
+                    style={{ marginTop: "16px", borderRadius: "8px" }}
                   >
-                    <LinkedinOutlined
-                      style={{ fontSize: "18px", marginRight: "4px" }}
-                    />
-                    <Text
-                      style={{ margin: 0, fontSize: "16px", color: "#fff" }}
-                    >
-                      LinkedIn
-                    </Text>{" "}
-                  </Link>
-                </div>
+                    LinkedIn
+                  </Button>
+                </Link>
               </Card>
             </Col>
           ))}
         </Row>
       </div>
-
-      {/* contact information */}
-      <section style={{ padding: "40px 20px" }}>
-        <Row
-          gutter={[16, 16]}
-          style={{
-            marginLeft: "10px",
-            marginRight: "10px",
-            background: "#4687E9",
-            borderRadius: "8px",
-            height: "100%",
-            alignItems: "center",
-            padding: "10px",
-          }}
-        >
-          <Col xs={24} md={12} style={{ textAlign: "left" }}>
-            <h6
-              style={{
-                fontSize: "22px",
-                color: "white",
-              }}
-            >
-              GET IN TOUCH
-            </h6>
-            <h5
-              style={{
-                fontSize: "30px",
-                margin: "10px 0",
-                color: "white",
-              }}
-            >
-              For Queries, Feedback or Assistance
-            </h5>
-            <Link to="/ContactUs">
-              <Button
-                type="primary"
-                size="large"
-                style={{
-                  marginTop: "10px",
-                  padding: "10px 20px",
-                  fontSize: "16px",
-                  background: "#fff",
-                  color: "#4687E9",
-                  fontWeight: "700",
-                }}
-              >
-                Contact Us
-              </Button>
-            </Link>
-          </Col>
-          <Col
-            xs={0}
-            md={12}
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              paddingLeft: "20px",
-            }}
-          >
-            <img
-              src={logo}
-              alt="Banner"
-              style={{
-                width: "100%",
-                maxWidth: "400px",
-                borderRadius: "5px",
-                objectFit: "cover",
-                marginLeft: "60px",
-              }}
-            />
-          </Col>
-        </Row>
-      </section>
     </>
   );
 }
